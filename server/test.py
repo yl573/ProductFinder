29fa772aad81a60d84b8495aabd538b8493c577f
yl573 <yl573@cam.ac.uk>
2018-01-20 11:14:27 +0000

    server only

---
+++ server/test.py
@@ -0,0 +1,33 @@
+from lib.product_search.main import ProductFinder
+from lib.database_builder.main import DatabaseBuilder
+from lib.qr_reader.main import QRReader
+
+from pymongo import MongoClient
+import pprint
+
+db_url='mongodb://localhost:27017/',
+db_name='ProductFinder'
+
+client = MongoClient(db_url)
+db = client[db_name]
+
+builder = DatabaseBuilder(db)
+builder.wipe_database()
+builder.load_store_from_folder('./TestStore')
+
+reader = QRReader(db)
+qr_codes = reader.find_codes_for_store('Coop')
+print('QR codes: ', qr_codes)
+
+store, transform = reader.get_info(qr_codes[0])
+print('store: ', store)
+print('QR code transform: ', transform)
+
+finder = ProductFinder(db)
+results = finder.find_matching_products('onion', store)
+print('results: ', pprint.pformat(results))
+
+if len(results) > 0:
+    path, height = finder.search_path_to_product(results[0], store, (0.1,3))
+    print('path: ', pprint.pformat(path))
+    print('height: ', height)
