ab6bb392b11cb1ef9c33c558abec0e71fa119549
Li Xi <xl404@cam.ac.uk>
2018-01-20 19:05:11 +0000

    added HTTP methods and is working YAY!!

---
+++ VuforiaApp/Assets/Scripts/SearchScrollListScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item {
+	public string itemName;
+
+}
+
+public class SearchScrollListScript : MonoBehaviour {
+
+	public List<Item> itemList;
+	public SimpleObjectPool itemObjectPool;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+}
