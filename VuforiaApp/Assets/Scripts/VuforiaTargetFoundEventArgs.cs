256e193033818e0e09a19e602e3c711d1693ec94
zhoudada <zhoudada123@hotmail.com>
2018-01-20 15:51:52 +0000

    Added Vuforia target found event args.

---
+++ VuforiaApp/Assets/Scripts/VuforiaTargetFoundEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VuforiaTargetFoundEventArgs : EventArgs
+{
+    public Transform TargetTransform { get; set; }
+
+    public VuforiaTargetFoundEventArgs(Transform targetTransform)
+    {
+        TargetTransform = targetTransform;
+    }
+}
