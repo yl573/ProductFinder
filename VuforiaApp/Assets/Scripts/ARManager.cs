39654f7f088c8fe3fec7a3f28aeb7044559f3c03
yl573 <yl573@cam.ac.uk>
2018-01-20 11:30:11 +0000

    added vuforia

---
+++ VuforiaApp/Assets/Scripts/ARManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARManager : MonoBehaviour {
+
+	public GameObject Vuforia;
+	public GameObject ARKit;
+
+	// Use this for initialization
+	void Start () {
+		ARKit.SetActive (false);
+		Vuforia.SetActive (true);
+	}
+
+	void OnGUI() {
+
+		if (GUI.Button (new Rect (10, 70, 50, 30), "Switch")) {
+			ARKit.SetActive (true);
+			Vuforia.SetActive (false);
+			Debug.Log ("switching");
+			// VuforiaBehaviour.Instance.enabled = false;
+		}
+	}
+
+}
