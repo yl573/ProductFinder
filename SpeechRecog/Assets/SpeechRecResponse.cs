e6533c0474fd7f9baf631e082d6d4acaff9ae3eb
Zoe Zhiyi Ma <mzy0369@gmail.com>
2018-01-20 23:26:50 +0000

    Add speech recognition support

---
+++ SpeechRecog/Assets/SpeechRecResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class SpeechRecResponse {
+	public string RecognitionStatus;
+	public string DisplayText;
+	public int Offset;
+	public int Duration;
+}
